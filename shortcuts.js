10b4213b3c851c9a56e7fbea9b38e154d64a7a90
Emily Lancaster <elancast@Princeton.edu>
2012-05-07 22:07:49 -0400

    first commit :D just got parsing keys working. TODO next: creating action functions for them

---
+++ shortcuts.js
@@ -0,0 +1,85 @@
+/******************************************************************************/
+// Emily Lancaster
+// shortcuts.js
+/******************************************************************************/
+
+/******************************************************************************/
+// Keyboard part of this project
+
+/*
+ * Handles a key press. Only pays attention if it is a keyboard shortcut not on
+ * an input text box.
+ */
+function handleKeyPress() {
+    if (event == null || event.keyIdentifier == null) {
+	console.error("Invalid keyup!");
+	return;
+    }
+
+    // Ignore key presses on the input search element
+    var tag = event.srcElement != null ? event.srcElement.tagName : null;
+    if (tag.toLowerCase() == 'input') {
+	console.log("IGNORING KEY PRESS ON INPUT");
+	return;
+    }
+
+    // Start the proper action
+    var keyId = event.keyIdentifier;
+    var key = keyId.toLowerCase() == "ENTER" ? 13 :
+	parseInt(keyId.substring(keyId.indexOf("+") + 1), 16);
+    switch (key) {
+    case 'j'.charCodeAt(0):
+    case 'J'.charCodeAt(0):
+	console.log("DOWN");
+	break;
+
+    case 'k'.charCodeAt(0):
+    case 'K'.charCodeAt(0):
+	console.log("UP");
+	break;
+
+    case 13:
+    case 'o'.charCodeAt(0):
+    case 'O'.charCodeAt(0):
+	console.log("ENTER");
+	break;
+
+    case '?'.charCodeAt(0):
+	console.log("QUESTION");
+	break;
+
+    default:
+	return;
+    }
+}
+
+/* Called upon start-up, injects listener for keys */
+function injectKeyListener() {
+    document.body.addEventListener('keyup', function() { handleKeyPress(); });
+}
+
+/******************************************************************************/
+// Parses Bing!
+
+/* Returns an array of results that could be iterated over via the keyboard */
+function findBingResults() {
+
+}
+
+/******************************************************************************/
+// Handles UI part of this project
+
+/* Removes focus from the search result */
+function removeFocus() {
+
+}
+
+/* Shows focus on the specific search result */
+function showFocus() {
+
+}
+
+/******************************************************************************/
+// Main...
+
+injectKeyListener();
